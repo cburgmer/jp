66512f33a9250ca491ddf86358543f250f48c6d3
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-05-31 22:49:37 +0200

    Pretty print json if no argument is given

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,27 @@ extern crate serde_json;
 use jsonpath::Selector;
 use serde_json::Value;
 
-fn main() {
-    let selector = env::args().nth(1)
-        .expect("Need a selector");
+fn do_query(selector: String, json: Value) {
     let selector = Selector::new(&selector)
         .expect("Unable to parse selector");
-
-    let json: Value = serde_json::from_reader(io::stdin())
-        .expect("Unable to parse JSON");
 
     selector.find(&json)
         .map(|m| m.to_string())
         .for_each(|e| println!("{}", e));
+}
+
+fn pretty_print(json: Value) {
+    let pretty_json = serde_json::to_string_pretty(&json)
+        .expect("Unable to format JSON");
+    println!("{}", pretty_json);
+}
+
+fn main() {
+    let json: Value = serde_json::from_reader(io::stdin())
+        .expect("Unable to parse JSON");
+
+    match env::args().nth(1) {
+        Some(selector) => do_query(selector, json),
+        None => pretty_print(json),
+    }
 }
