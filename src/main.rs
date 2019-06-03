3cd1692efc0ded3d888b5e08d34f2d94ebe95e13
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-06-03 21:46:42 +0200

    Return JSON by default. Isomorphic operation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::io;
-use std::env;
 extern crate jsonpath;
 extern crate serde_json;
+extern crate clap;
 
+use clap::{Arg, App};
 use jsonpath::Selector;
 use serde_json::Value;
 
-fn do_query(selector: String, json: Value) {
-    let selector = Selector::new(&selector)
+fn do_query(selector: &str, json: Value, show_raw: bool) {
+    let selector = Selector::new(selector)
         .expect("Unable to parse selector");
 
-    selector.find(&json)
-        .try_for_each(|m| serde_json::to_writer(io::stdout(), &m))
-        .expect("Unable to serialize JSON");
+    let matches = selector.find(&json);
+
+    if show_raw {
+        matches
+            .map(|m| serde_json::to_string(&m).unwrap())
+            .for_each(|m| {
+                println!("{}", m);
+            });
+    } else {
+        let m: Vec<&Value> = matches.collect();
+        serde_json::to_writer(io::stdout(), &m)
+            .expect("Unable to serialize JSON");
+    }
 }
 
 fn pretty_print(json: Value) {
@@ -21,11 +32,23 @@ fn pretty_print(json: Value) {
 }
 
 fn main() {
+    let matches = App::new("jp")
+        .version("0.0.1")
+        .about("jq but with JSONPath")
+        .arg(Arg::with_name("r")
+             .short("r")
+             .help("Returns one entry per line"))
+        .arg(Arg::with_name("SELECTOR")
+             .help("JSONPath selector")
+             .index(1))
+        .get_matches();
+
     let json: Value = serde_json::from_reader(io::stdin())
         .expect("Unable to parse JSON");
 
-    match env::args().nth(1) {
-        Some(selector) => do_query(selector, json),
-        None => pretty_print(json),
+    if matches.is_present("SELECTOR") {
+        do_query(matches.value_of("SELECTOR").unwrap(), json, matches.is_present("r"))
+    } else {
+        pretty_print(json);
     }
 }
